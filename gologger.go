382a714579826413c7cb448f80f6f978ebe14cb5
Shihab Chowdhury <sachow168@gmail.com>
2020-04-12 10:59:36 -0400

    Bug fix

--- gologger.go
+++ gologger.go
@@ -16,21 +16,21 @@ func GetTime() string {
 }
 
 func (l *Logger) Green(msg string, a ...interface{}) {
-	color.Green("["+GetTime()+"] "+" ["+l.Name+"] [+]"+msg, a...)
+	color.Green("["+GetTime()+"] "+"["+l.Name+"] [+] "+msg, a...)
 }
 
 func (l *Logger) Blue(msg string, a ...interface{}) {
-	color.Blue("["+GetTime()+"] "+" ["+l.Name+"] [$]"+msg, a...)
+	color.Blue("["+GetTime()+"] "+"["+l.Name+"] [$] "+msg, a...)
 }
 
 func (l *Logger) Red(msg string, a ...interface{}) {
-	color.Red("["+GetTime()+"] "+" ["+l.Name+"] [x]"+msg, a...)
+	color.Red("["+GetTime()+"] "+"["+l.Name+"] [x] "+msg, a...)
 }
 
 func (l *Logger) Yellow(msg string, a ...interface{}) {
-	color.Yellow("["+GetTime()+"] "+" ["+l.Name+"] [?]"+msg, a...)
+	color.Yellow("["+GetTime()+"] "+"["+l.Name+"] [?] "+msg, a...)
 }
 
 func (l *Logger) Normal(msg string, a ...interface{}) {
-	color.White("["+GetTime()+"] "+" ["+l.Name+"] [#]"+msg, a...)
+	color.White("["+GetTime()+"] "+"["+l.Name+"] [#] "+msg, a...)
 }
